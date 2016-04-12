3ade83dbe6643c11278665ba6d9d03a87eb1c983
Vladimir Masko <soupdawgie@gmail.com>
2016-04-12 17:12:29 +0300

    Fixed action links aligning issue

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,5 +1,6 @@
 class Query < ActiveRecord::Base
   validates :amount, :vendor, :start, :separator, presence: true
   validates :vendor, :start, length: { is: 6 }
-    
+  default_scope -> { order(created_at: :desc) }
+
 end
